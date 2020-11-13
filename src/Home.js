8a8d07d1452c5bc1c7b833e3de3ead8c824187ef
Seda Mete <metes1@mcmaster.ca>
2020-11-12 20:15:34 -0500

    added search and browse page routes in home, created sortby component

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,20 @@ import book from "./book.jpg";
 import Title from "./Homepage/Title";
 import Card1 from "./Homepage/Card1";
 import Sell from "./Sell/Sell";
-
+import Search from "./Search/Search";
+import Browse from "./Browse/Browse";
 import "./Home.css";
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// Files contain data for browse books page
+import bdata from "./Browse/data/bdata.json";
+import edata from "./Browse/data/edata.json";
+import sdata from "./Browse/data/sdata.json";
+import hdata from "./Browse/data/hdata.json";
+import ssdata from "./Browse/data/ssdata.json";
+import hsdata from "./Browse/data/hsdata.json";
+    
 
 const Home = (props) => {
   return (
@@ -25,35 +34,45 @@ const Home = (props) => {
                         Home
                       </Link>
                     </li>
+                    <li>
+                      <Link to="/search" className="link">
+                        Search
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/sell" className="link">
+                        Sell
+                      </Link>
+                    </li>
                     <li>Browse Subject</li>
                     <ul>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/business" className="link">
                           Business
                         </Link>
                       </li>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/engineering" className="link">
                           Engineering
                         </Link>
                       </li>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/science" className="link">
                           Science
                         </Link>
                       </li>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/healthscience" className="link">
                           Health Science
                         </Link>
                       </li>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/humanities" className="link">
                           Humanities
                         </Link>
                       </li>
                       <li>
-                        <Link to="/browse" className="link">
+                        <Link to="/browse/socialscience" className="link">
                           Social Science
                         </Link>
                       </li>
@@ -64,8 +83,26 @@ const Home = (props) => {
             </Col>
             <Col xs={10} id="page-content-wrapper">
               <Switch>
-                <Route path="/browse">
-                  <p>c</p>{" "}
+                <Route path="/search">
+                  <Search />
+                </Route>
+                <Route path="/browse/business">
+                  <Browse subject="Business" file={bdata}/>
+                </Route>
+                <Route path="/browse/engineering">
+                  <Browse subject="Engineering" file={edata}/>
+                </Route>
+                <Route path="/browse/science">
+                  <Browse subject="Science" file={sdata}/>
+                </Route>
+                <Route path="/browse/healthscience">
+                  <Browse subject="Health Science" file={hsdata}/>
+                </Route>
+                <Route path="/browse/humanities">
+                  <Browse subject="Humanities" file={hdata}/>
+                </Route>
+                <Route path="/browse/socialscience">
+                  <Browse subject="Social Science" file={ssdata}/>
                 </Route>
                 <Route path="/sell">
                   <Sell />
@@ -75,9 +112,11 @@ const Home = (props) => {
                     <Image className="image2" src={book} />
                   </div>
                   <div style={{ clear: "both" }} className="buttonz">
-                    <Button variant="warning" size="lg">
-                      <h3>&nbsp; Buy &nbsp;</h3>
-                    </Button>
+                    <Link to="/search" className="link">
+                      <Button variant="warning" size="lg">
+                        <h3>&nbsp; Buy &nbsp;</h3>
+                      </Button>
+                    </Link>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <Link to="/sell" className="link">
                       <Button variant="warning" size="lg">
@@ -95,4 +134,5 @@ const Home = (props) => {
     </Router>
   );
 };
+
 export default Home;
